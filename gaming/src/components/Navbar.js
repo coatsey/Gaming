a93c1c58c35e28f3d4252b7a7a8f0ac1929b341c
Logan <coates.logan1125@gmail.com>
2021-10-20 14:32:55 -0600

    getting started

---
+++ gaming/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { Button } from './Button'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+import Dropdown from './Dropdown'
+
+function Navbar() {
+const [click, setClick] = useState(false)
+
+const handleClick = () => setClick(!click)
+
+    return (
+        <>
+            <nav className='navbar'>
+              <Link to='/' className='navbar-logo'>
+                  Loadouts
+                  </Link> 
+                  <div className='menu-icon' onClick={handleClick}>
+                      <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                  </div>
+            </nav>
+        </>
+    )
+}
+
+
+export default Navbar;
